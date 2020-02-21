9992e6739472aa4a388b76aca896b3656d6cf32e
GiorgosStakias <gstakias6898@gmail.com>
2020-02-21 20:15:28 +0200

    Updating back-end-v1.1

--- back-end/src/main/java/gr/ntua/ece/softeng19b/data/DataAccess.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/data/DataAccess.java
@@ -66,18 +66,24 @@ public class DataAccess {
         };
 
         //TODO: Insert a valid SQL query
-        String sqlQuery = "select x from t1 where " +
-                          "AreaName = ? and Resolution = ? and Year = ? and Month = ? and Day = ?";
+        String sqlQuery = "select atl.areaname, atc.areatypecodetext, mc.mapcodetext, rc.resolutioncodetext, atl.year, atl.month, atl.day, atl.TotalLoadValue "+
+                          "from actualtotalload as atl, resolutioncode as rc, areatypecode as atc, mapcode as mc " +
+                          "where atl.areaname=? and rc.resolutioncodetext=? and atl.Year=? and atl.Month=? and atl.Day=? " +
+                          "and rc.Id=atl.ResolutionCodeId and mc.id=atl.mapcodeid and atc.id=atl.AreaTypeCodeId";
+		try {
+					return jdbcTemplate.query(sqlQuery, sqlParams, (ResultSet rs, int rowNum) -> {
+						ATLRecordForSpecificDay dataLoad = new ATLRecordForSpecificDay();
+						dataLoad.setAreaName(rs.getString(1)); //get the string located at the 1st column of the result set
+						dataLoad.setAreaTypeCode(rs.getString(2)); //get the int located at the 2nd column of the result set
+						dataLoad.setMapCode(rs.getString(3));
+						dataLoad.setResolutionCode(rs.getString(4));
+						dataLoad.setYear(rs.getInt(5));
+						dataLoad.setMonth(rs.getInt(6));
+						dataLoad.setDay(rs.getInt(7));
+						dataLoad.setActualTotalLoadValue(rs.getDouble(8));
+						return dataLoad;
 
-        try {
-            return jdbcTemplate.query(sqlQuery, sqlParams, (ResultSet rs, int rowNum) -> {
-                ATLRecordForSpecificDay dataLoad = new ATLRecordForSpecificDay();
-                dataLoad.setAreaName(rs.getString(1)); //get the string located at the 1st column of the result set
-                dataLoad.setYear(rs.getInt(2)); //get the int located at the 2nd column of the result set
-                //...
-                return dataLoad;
-
-            });
+					});
         }
         catch(Exception e) {
             throw new DataAccessException(e.getMessage(), e);
