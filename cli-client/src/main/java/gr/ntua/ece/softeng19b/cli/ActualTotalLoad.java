92ea79d5288de752226d7fcfb6614daaafd26d27
john98nf <giannhs.afasias@gmail.com>
2020-02-26 14:13:02 +0200

    Functional ATL for Day/Month/Year with JSON representation

--- cli-client/src/main/java/gr/ntua/ece/softeng19b/cli/ActualTotalLoad.java
+++ cli-client/src/main/java/gr/ntua/ece/softeng19b/cli/ActualTotalLoad.java
@@ -40,9 +40,14 @@ public class ActualTotalLoad extends EnergyCliArgs implements Callable<Integer> 
                 List<ATLRecordForSpecificDay> records = new RestAPI().
                         getActualTotalLoad(areaName, timeres.name(), LocalDate.parse(dateArgs.date), format);
                 // Do something with the records :)
+                if (records.isEmpty() == true) {
+                    System.out.println("Fetched 0 records");
+                    return 0;
+                }
                 JsonWriter w = new JsonWriter(new OutputStreamWriter(System.out));
                 w.setIndent("  ");
-                w.beginArray();        //System.out.print("[");
+                w.beginArray();
+                //System.out.print("[ ");
                 for(ATLRecordForSpecificDay rec : records){
                     w.beginObject(); // {
                     w.name("Source").value(rec.getSource());
@@ -62,7 +67,8 @@ public class ActualTotalLoad extends EnergyCliArgs implements Callable<Integer> 
                     }*/
                 }
                 w.endArray();
-                w.flush();              //System.out.println("]");
+                w.flush();
+                //System.out.println("]");
                 System.out.println();
                 System.out.println("Fetched " + records.size() + " records");
                 return 0;
@@ -70,14 +76,61 @@ public class ActualTotalLoad extends EnergyCliArgs implements Callable<Integer> 
             else if (dateArgs.month != null){
                 List<ATLRecordForSpecificMonth> records = new RestAPI().
                         getActualTotalLoad(areaName, timeres.name(), YearMonth.parse(dateArgs.month), format);
-         
+                if (records.isEmpty() == true) {
+                    System.out.println("Fetched 0 records");
+                    return 0;
+                }
+                JsonWriter w = new JsonWriter(new OutputStreamWriter(System.out));
+                w.setIndent("  ");
+                w.beginArray();
+                for(ATLRecordForSpecificMonth rec : records){
+                    w.beginObject(); // {
+                    w.name("Source").value(rec.getSource());
+                    w.name("DataSet").value(rec.getDataSet());
+                    w.name("AreaName").value(rec.getAreaName());
+                    w.name("AreaTypeCode").value(rec.getAreaTypeCode());
+                    w.name("MapCode").value(rec.getMapCode());
+                    w.name("ResolutionCode").value(rec.getResolutionCode());
+                    w.name("Year").value(rec.getYear());
+                    w.name("Month").value(rec.getMonth());
+                    w.name("Day").value(rec.getDay());
+                    w.name("ActualTotalLoadByDayValue").value(rec.getActualTotalLoadByDayValue());
+                    w.endObject(); // }
+                    w.flush();
+                }
+                w.endArray();
+                w.flush();
+                System.out.println();
                 System.out.println("Fetched " + records.size() + " records");
                 return 0;
             }
             else {
                 List<ATLRecordForSpecificYear> records = new RestAPI().
                         getActualTotalLoad(areaName, timeres.name(), Year.parse(dateArgs.year), format);
-         
+                if (records.isEmpty() == true) {
+                    System.out.println("Fetched 0 records");
+                    return 0;
+                }
+                JsonWriter w = new JsonWriter(new OutputStreamWriter(System.out));
+                w.setIndent("  ");
+                w.beginArray();
+                for(ATLRecordForSpecificYear rec : records){
+                    w.beginObject(); // {
+                    w.name("Source").value(rec.getSource());
+                    w.name("DataSet").value(rec.getDataSet());
+                    w.name("AreaName").value(rec.getAreaName());
+                    w.name("AreaTypeCode").value(rec.getAreaTypeCode());
+                    w.name("MapCode").value(rec.getMapCode());
+                    w.name("ResolutionCode").value(rec.getResolutionCode());
+                    w.name("Year").value(rec.getYear());
+                    w.name("Month").value(rec.getMonth());
+                    w.name("ActualTotalLoadByMonthValue").value(rec.getActualDataLoadByMonthValue());
+                    w.endObject(); // }
+                    w.flush();
+                }
+                w.endArray();
+                w.flush();
+                System.out.println();
                 System.out.println("Fetched " + records.size() + " records");
                 return 0;
 
