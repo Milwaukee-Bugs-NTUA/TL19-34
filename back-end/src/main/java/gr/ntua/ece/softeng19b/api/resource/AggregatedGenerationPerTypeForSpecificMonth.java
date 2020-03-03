9671037241c3c0de75a4d5a4eaa09cac36a9a936
GiorgosStakias <gstakias6898@gmail.com>
2020-03-03 07:29:46 +0200

    Added error 402 to all get data calls because I forgot to do it earlier and xanax has to make an income

--- back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/AggregatedGenerationPerTypeForSpecificMonth.java
+++ back-end/src/main/java/gr/ntua/ece/softeng19b/api/resource/AggregatedGenerationPerTypeForSpecificMonth.java
@@ -20,58 +20,54 @@ import java.util.Base64;
 public class AggregatedGenerationPerTypeForSpecificMonth extends EnergyResource {
 
     private final DataAccess dataAccess = Configuration.getInstance().getDataAccess();
+    private static Status outOfQuotasStatus = new Status(402, "Out of Quotas");
 
     @Override
     protected Representation get() throws ResourceException {
 
-        //Read the mandatory URI attributes
-        
+        // Read the mandatory URI attributes
+
         String h = getRequest().getHeaders().toString();
         String token = null;
 
-        try{
+        try {
             token = getToken(h);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             throw new ResourceException(Status.CLIENT_ERROR_UNAUTHORIZED, e.getMessage(), e);
         }
 
-        String userName = Jwts.parser()         
-        .setSigningKey(Base64.getDecoder().decode("J0KlwfLrnZ92TWJ0VgZXZjTAnQynDpnYY4TYdBTvtOc="))
-        .parseClaimsJws(token).getBody().get("username").toString();                         
+        String userName = Jwts.parser()
+                .setSigningKey(Base64.getDecoder().decode("J0KlwfLrnZ92TWJ0VgZXZjTAnQynDpnYY4TYdBTvtOc="))
+                .parseClaimsJws(token).getBody().get("username").toString();
 
         String areaName = getMandatoryAttribute("AreaName", "AreaName is missing");
         String resolution = getMandatoryAttribute("Resolution", "Resolution is missing");
         String productionType = getMandatoryAttribute("ProductionType", "ProductionType is missing");
-        //Read the optional date attribute
+        // Read the optional date attribute
         String dateParam = getAttributeDecoded("month");
         String values[] = dateParam.split("-");
         int year = Integer.parseInt(values[0]);
         int month = Integer.parseInt(values[1]);
-        
-        //Use the EnergyResource.parseXXX methods to parse the dates and implement the required business logic
-        //For the sake of this example, we hard-code a date
+
+        // Use the EnergyResource.parseXXX methods to parse the dates and implement the
+        // required business logic
+        // For the sake of this example, we hard-code a date
         YearMonth yearmonth = YearMonth.of(year, month);
 
-        //Read the format query parameter
+        // Read the format query parameter
         Format format = parseFormat(getQueryValue("format"));
 
         try {
 
             List<AGPTRecordForSpecificMonth> result = dataAccess.fetchAggregatedGenerationPerTypeForSpecificMonth(
-                    areaName,
-                    resolution,
-                    productionType,
-                    yearmonth,
-                    userName
-            );
+                    areaName, resolution, productionType, yearmonth, userName);
             return format.generateRepresentationAGPTFSM(result);
         } catch (Exception e) {
+            if (e.getMessage().equals("Out of Quotas (402) - Out of Quotas!"))
+                throw new ResourceException(outOfQuotasStatus, e.getMessage(), e);
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
 
     }
-
 
 }
